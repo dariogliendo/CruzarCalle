c9246ac2fbbfdfba6e75d16d992f2fac482eea18
VladimirCharkot <vlad.chk@gmail.com>
2024-06-15 16:55:59 -0300

    Refactor :sparkles:

--- scripts/vehiculo.gd
+++ scripts/vehiculo.gd
@@ -1,14 +1,36 @@
 extends Node2D
 
-var velocidad = 1;
-var direccion = 1
-var rng = RandomNumberGenerator.new();
-var stopped = false;
+class_name Vehiculo
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+var velocidad = 1
+var direccion = 1
+var stopped = false
+
+signal sale(vehiculo)
+
+# Lo ubica fuera de la pantalla, izq o der con 50% chance
+func init_offscreen():
+	var desde_izquierda = randf() < 0.5		# Chance 50%
+	var viewport = get_viewport_rect()
+	if desde_izquierda:
+		position.x = -100;
+		direccion = 1
+	else:
+		position.x = viewport.size.x + 100
+		direccion = -1
+	scale.x = direccion
+
+func _ready():
+	init_offscreen()
+
 func _process(delta):
-	var window = get_window()
+	
+	# Movimiento
 	if not stopped:
 		position.x += velocidad * direccion
-	if position.x < -100 or position.x > window.size.x + 100:
+	
+	# Si sale de la pantalla, lo eliminamos
+	var viewport = get_viewport_rect()
+	if position.x < -100 or position.x > viewport.size.x + 100:
+		sale.emit(self)		# Avisamos pa'fuera que se va
 		queue_free()
